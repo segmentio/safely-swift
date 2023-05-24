3f15af33e1842a8121d9f5f4cd6c977dab11f0f8
Alan Charles <50601149+alanjcharles@users.noreply.github.com>
2023-05-24 09:17:08 -0600

    feat: add version file for release (#3)
    Co-authored-by: Alan Charles <acharles@twilio.com>

---
+++ Version.swift
@@ -0,0 +1,11 @@
+// Referred to by Safely.swift
+
+// DO NOT MODIFY THIS FILE BY HAND!!
+// DO NOT MODIFY THIS FILE BY HAND!!
+// DO NOT MODIFY THIS FILE BY HAND!!
+// DO NOT MODIFY THIS FILE BY HAND!!
+
+// Use release.sh's automation.
+
+// BREAKING.FEATURE.FIX
+internal let __safely_version = "1.0.0"
